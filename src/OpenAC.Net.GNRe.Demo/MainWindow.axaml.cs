64245ebf50ec6cfd9944038f9da5197c7f601014
Rafael Teno Dias <rfteno@gmail.com>
2026-08-20 21:15:44 -0400

    refactor(demo): substitui Watermark por PlaceholderText
    - Atualiza `Watermark` para `PlaceholderText` em InputDialog.axaml e MainWindow.axaml
    - Adequa os controles TextBox para a API atual do Avalonia UI

--- src/OpenAC.Net.GNRe.Demo/MainWindow.axaml.cs
+++ src/OpenAC.Net.GNRe.Demo/MainWindow.axaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using Avalonia.Controls;
+using Avalonia.Input.Platform;
 using Avalonia.Platform.Storage;
 using OpenAC.Net.Core.Extensions;
 using OpenAC.Net.DFe.Core.Common;
